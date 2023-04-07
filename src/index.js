7d7798869ded7981ea3511abe2659d262cb1f91a
Chiara Bauni <cbauni@fi.uba.ar>
2023-04-07 18:10:08 -0300

    fix index

--- src/index.js
+++ src/index.js
@@ -4,26 +4,7 @@ import './index.css';
 import App from './App';
 import { firebase } from './providers/FirebaseProvider';
 import UserProvider from './providers/UserProvider';
-
 export const FirebaseContext = React.createContext({});
-
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-
-export const FirebaseContext = React.createContext({});
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: "ticketapp-public.firebaseapp.com",
-    projectId: "ticketapp-public",
-    storageBucket: "ticketapp-public.appspot.com",
-    messagingSenderId: "281878738338",
-    appId: process.env.FIREBASE_APPID
-};
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-const firebase = {storage}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -32,7 +13,6 @@ root.render(
             <UserProvider>
                 <App />
             </UserProvider>
-            <App />
         </FirebaseContext.Provider>
     </React.StrictMode>
 );
