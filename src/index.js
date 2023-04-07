0869a621a43f7fc75ca2a90daa72c926c335cb58
Gianbelinche <gbelinche@fi.uba.ar>
2023-04-07 17:53:34 -0300

    Fix error

--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { firebase } from './providers/FirebaseProvider';
-import UserProvider from './providers/UserProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { firebase } from "./providers/FirebaseProvider";
+import UserProvider from "./providers/UserProvider";
 
 export const FirebaseContext = React.createContext({});
 
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-
-export const FirebaseContext = React.createContext({});
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: "ticketapp-public.firebaseapp.com",
-    projectId: "ticketapp-public",
-    storageBucket: "ticketapp-public.appspot.com",
-    messagingSenderId: "281878738338",
-    appId: process.env.FIREBASE_APPID
-};
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-const firebase = {storage}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <FirebaseContext.Provider value={firebase}>
-            <UserProvider>
-                <App />
-            </UserProvider>
-            <App />
-        </FirebaseContext.Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <FirebaseContext.Provider value={firebase}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+      <App />
+    </FirebaseContext.Provider>
+  </React.StrictMode>
 );
