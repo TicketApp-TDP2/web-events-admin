d563e323c20e7164509439767bbea6860333d82b
Dani Leloutre <39065230+dleloutre@users.noreply.github.com>
2023-04-10 13:48:00 -0300

    Update FirebaseProvider.js

--- src/providers/FirebaseProvider.js
+++ src/providers/FirebaseProvider.js
@@ -5,10 +5,10 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: "ticketapp-organizer.firebaseapp.com",
-    projectId: "ticketapp-organizer",
-    storageBucket: "ticketapp-organizer.appspot.com",
-    messagingSenderId: "565725159820",
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
