7d393dde8f731bb0923face34c1114d8649f8bc2
Daniela Leloutre <daniela@woowup.com>
2023-05-05 22:13:22 -0300

    agrego funciones para mandar push

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { firebase } from "./providers/FirebaseProvider";
+import { mobileNotificationsFirebase } from "./providers/MobileNotificationsProvider";
 import UserProvider from "./providers/UserProvider";
 
 export const FirebaseContext = React.createContext({});
+export const MobileNotificationsContext = React.createContext({});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={firebase}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+        <MobileNotificationsContext.Provider value={mobileNotificationsFirebase}>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </MobileNotificationsContext.Provider>
     </FirebaseContext.Provider>
   </React.StrictMode>
 );
