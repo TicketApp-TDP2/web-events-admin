4befd92a39daec7b185b27e64e0017c69524d13c
Daniela Leloutre <daniela@woowup.com>
2023-05-27 19:10:10 -0300

    fix boton salir

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,7 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Link, Typography,
+  Link, Typography, Grid, Box, Stack,
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
@@ -33,70 +33,68 @@ export default function SideBar() {
   ];
 
   const getList = () => (
-    <>
-      {data.map((item, index) => (
-        <Link
-          underline="none"
-          component={RouterLink}
-          to={item.navigate}
-          key={index}
-        >
-          <ListItem
-            key={index}
-            selected={pathname === item.navigate}
-            sx={{ color: "#fff" }}
-          >
-            <ListItemIcon sx={{ color: "#fff" }}>
-              {item.icon}
-            </ListItemIcon>
-            <ListItemText primary={item.name}/>
-          </ListItem>
-        </Link>
-      ))}
-    </>
+      <div>
+        {data.map((item, index) => (
+            <Link
+                underline="none"
+                component={RouterLink}
+                to={item.navigate}
+                key={index}
+            >
+              <ListItem
+                  key={index}
+                  selected={pathname === item.navigate}
+                  sx={{ color: "#fff" }}
+              >
+                <ListItemIcon
+                    sx={{ color: "#fff" }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.name}/>
+              </ListItem>
+            </Link>
+        ))}
+      </div>
   );
 
   return (
-    <>
-      <RootStyle>
-        <Drawer
-          PaperProps={{
-            sx: {
-              backgroundColor: '#8978C7',
-              color: '#fff',
-              borderRightStyle: 'dashed',
-              width: DRAWER_WIDTH,
-            },
-          }}
-          sx={{
-            '&& .Mui-selected, && .Mui-selected:hover': {
-              bgcolor: '#6C5483',
-              border: '1px solid #FFFFFF',
-              borderRadius: '10px'
-            }
-          }}
-          variant="permanent"
-          anchor="left"
-          open
-        >
-          <img src="/Logo2.png" alt="logo" />
-          {getList()}
-        </Drawer>
-        {
-          /*
-              { name: "Salir", icon: <LogoutIcon onClick={logout} />, navigate: '/' },
-           */
-        }
-        <Link
-            underline="none"
-            component={RouterLink}
-            to="/"
-        >
-          <LogoutIcon onClick={logout} color="#fff" />
-          <Typography color="#fff">Salir</Typography>
-        </Link>
-      </RootStyle>
-    </>
+      <>
+        <RootStyle>
+          <Drawer
+              PaperProps={{
+                sx: {
+                  backgroundColor: '#8978C7',
+                  color: '#fff',
+                  borderRightStyle: 'dashed',
+                  width: DRAWER_WIDTH,
+                },
+              }}
+              sx={{
+                '&& .Mui-selected, && .Mui-selected:hover': {
+                  bgcolor: '#6C5483',
+                  border: '1px solid #FFFFFF',
+                  borderRadius: '10px'
+                }
+              }}
+              variant="permanent"
+              anchor="left"
+              open
+          >
+            <img src="/Logo2.png" alt="logo" />
+            {getList()}
+            <Link
+                underline="none"
+                component={RouterLink}
+                to="/"
+            >
+              <Stack direction="row" spacing={2} sx={{ margin: 2, position: 'absolute', bottom: 0 }}>
+                <LogoutIcon onClick={logout} sx={{ color: "white" }} />
+                <Typography color="#fff">Salir</Typography>
+              </Stack>
+            </Link>
+          </Drawer>
+        </RootStyle>
+      </>
   );
 }
-
