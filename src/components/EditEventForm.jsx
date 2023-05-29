84ef74faedd73946de0b7caa3adcaadcf70e2f6a
Chiara Bauni <chiarabauni@MacBook-Pro-de-Chiara.local>
2023-05-29 16:21:16 -0300

    Agrego casos de errores

--- src/components/EditEventForm.jsx
+++ src/components/EditEventForm.jsx
@@ -347,6 +347,15 @@ export default function EditEventForm(props) {
                     case "agenda_can_not_end_after_event_end":
                         errorText = "La agenda no puede terminar despues del final del evento"
                         break;
+                    case "end_time_must_be_greater_than_start_time":
+                        errorText = "La hora de finalización debe ser mayor a la hora de inicio"
+                        break;
+                    case "agenda_can_not_end_before_event_ends":
+                        errorText = "La agenda no puede terminar antes del final del evento"
+                        break;
+                    case "time_can_not_be_updated_without_agenda":
+                        errorText = "No se puede actualizar el horario del evento sin editar la agenda"
+                        break;
                     default:
                         errorText = "Ocurrió un error inesperado. Porfavor vuelva a internar mas tarde"
                         break;
